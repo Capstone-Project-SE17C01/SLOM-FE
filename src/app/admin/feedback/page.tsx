415f11422c152fb9d1f63cd565631c45f14a368a
Quan Phung <hnbhanh080803@gmail.com>
2025-08-11 21:00:05 +0700

    content

--- src/app/admin/feedback/page.tsx
+++ src/app/admin/feedback/page.tsx
@@ -1,7 +1,7 @@
-// "use client";
+"use client";
 
-// import AdminFeedback from "@/components/layouts/admin/admin-feedback";
+import AdminFeedback from "@/components/layouts/admin/admin-feedback";
 
-// export default function FeedbackPage() {
-//   return <AdminFeedback />;
-// }
+export default function FeedbackPage() {
+  return <AdminFeedback />;
+}
